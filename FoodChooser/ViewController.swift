a3baf6556fb7316970ff3619de30702a54bc8905
Yl <Yl@naver.com>
2021-04-11 15:51:04 +0900

    Initial Commit

---
+++ FoodChooser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodChooser
+//
+//  Created by Yundong Lee on 2021/04/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
